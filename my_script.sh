928d02ee0eb3ec0715ed88f48769ba4a3d78da04
Warpinguin <72430774+Warpinguin@users.noreply.github.com>
2021-04-09 14:40:52 +0200

    Update my_script.sh

--- my_script.sh
+++ my_script.sh
@@ -4,7 +4,9 @@ echo \"Zacatek / Start\" 
 
 pkg update && pkg upgrade -y && termux-setup-storage && pkg install termux-api && pkg install git python python2 curl root-repo unstable-repo x11-repo vim termux-tools wget moreutils perl coreutils nodejs texinfo tsu coreutils diffutils libtool make perl clang && pkg install ncurses-utils -y && python3 -m pip install --upgrade pip && python2 -m pip install --upgrade pip && python3 -m pip install requests wheel future && pip install requests wheel future && pkg update && pkg upgrade -y 
 
-echo \"done\" 
+echo *
+
+echo \"done\" 
 
 echo exit ;
 
