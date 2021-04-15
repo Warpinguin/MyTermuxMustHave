85f01022c7b5af4ad7bdb8125cc38a39fbced143
Warpinguin <72430774+Warpinguin@users.noreply.github.com>
2021-04-15 09:07:58 +0200

    Add files via upload

--- my_script.sh
+++ my_script.sh
@@ -2,11 +2,9 @@
 
 echo \"Zacatek / Start\" 
 
-pkg update && pkg upgrade -y && termux-setup-storage && pkg install termux-api && pkg install git python python2 curl root-repo unstable-repo x11-repo vim termux-tools wget moreutils perl coreutils nodejs texinfo tsu coreutils diffutils libtool make perl clang && pkg install ncurses-utils -y && python3 -m pip install --upgrade pip && python2 -m pip install --upgrade pip && python3 -m pip install requests wheel future && pip install requests wheel future && pkg update && pkg upgrade -y 
+apt update && apt upgrade -y && termux-setup-storage && apt install termux-api && apt install git python python2 curl root-repo unstable-repo x11-repo vim termux-tools wget moreutils perl coreutils nodejs texinfo tsu coreutils diffutils libtool make perl clang && apt install ncurses-utils -y && python3 -m pip install --upgrade pip && python2 -m pip install --upgrade pip && python3 -m pip install requests wheel future && pip install requests wheel future && python2 -m pip install mechanize && python2 -m pip install requests wheel future && pip install requests wheel future && python3 -m pip install mechanize && apt update && apt upgrade -y 
 
-echo *
-
-echo \"done\" 
+echo \"done\" 
 
 echo exit ;
 
